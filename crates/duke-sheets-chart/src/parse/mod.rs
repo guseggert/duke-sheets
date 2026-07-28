92b86f2c417ddb6e7b95ca4828adf6a78e056f4d
Gus Eggert <gus@gus.dev>
2026-07-28 13:18:01 +0000

    Pin what mutation testing found unpinned

--- crates/duke-sheets-chart/src/parse/mod.rs
+++ crates/duke-sheets-chart/src/parse/mod.rs
@@ -4,4 +4,4 @@ mod chart_style;
 
 pub use chart::parse_chart_xml;
 pub use chart_ex::parse_chart_ex_xml;
-pub use chart_style::{parse_chart_color_style, parse_chart_style};
+pub use chart_style::{chart_style_sequence, parse_chart_color_style, parse_chart_style};
